03b27e5b576a2d97d6b56589584703d4b8b857ae
ppom <>
2025-06-02 12:00:00 +0200

    Allow for video random order

--- client/src/app/shared/shared-video-miniature/video-filters-header.component.ts
+++ client/src/app/shared/shared-video-miniature/video-filters-header.component.ts
@@ -151,6 +151,7 @@ export class VideoFiltersHeaderComponent implements OnInit {
 
   private buildSortItems () {
     this.sortItems = [
+      { id: 'random', label: $localize`Random` },
       { id: '-publishedAt', label: $localize`Recently Added` },
       { id: '-originallyPublishedAt', label: $localize`Original Publication Date` },
       { id: 'name', label: $localize`Name` }
